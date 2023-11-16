ae47bf771607be7f395d206168a23e1cf77c6f1f
Ayshu-devi <150216493+Ayshu-devi@users.noreply.github.com>
2023-11-16 16:25:45 +0530

    DAY4

---
+++ day4/reactapp1/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ListItems from './components/file/ListItems';
+import EmployeeDetails from './components/EmployeeDetails';
+function App() {
+  return (
+    <div className="App">
+      <ListItems/>
+      <EmployeeDetails/>
+    </div>
+  );
+}
+
+export default App;
