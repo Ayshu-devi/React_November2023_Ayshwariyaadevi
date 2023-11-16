002bff1acea82910be6020319e0eee3051e03717
Ayshu-devi <150216493+Ayshu-devi@users.noreply.github.com>
2023-11-16 16:24:47 +0530

    DAY3

---
+++ day3/reactapp2/src/components/Toogle.js
@@ -0,0 +1,23 @@
+import React,{useState} from 'react'
+function Toogle(){
+  const[name,setName]=useState("kaioken");
+  const [count,setCount]=useState(0);
+  const transformationHandler=()=>{
+    let value=count;
+    setCount(++value);
+    if(count===3){
+        if(name==="Kaioken") setName("SuperSaiyan");
+        else setName("Kaioken");
+        setCount(0);
+    }
+  };
+  return (
+    <div>
+        <h1>Toogle</h1>
+        <hr></hr>
+        <h1>{name}</h1>
+        <button onClick={transformationHandler}>Toogle</button>
+    </div>
+);
+}
+export default Toogle;
---
+++ day3/reactapp3/src/components/Togglemessage.js
@@ -0,0 +1,18 @@
+import React,{useState} from "react";
+function Togglemessage(){
+ const [showMessage,setShowMessage]=useState(false);
+ const Togglemessage=()=>{
+  setShowMessage(!showMessage);
+ };
+ return(
+    <div>
+        <h1>Dashboard</h1>
+        <hr></hr>
+        <button onClick={Togglemessage}>
+        {showMessage ? "Show Message" : "Hide Message"}
+        </button>
+        {showMessage && <p>Hi! How are you</p>}
+    </div>
+ );
+}
+export default Togglemessage;
