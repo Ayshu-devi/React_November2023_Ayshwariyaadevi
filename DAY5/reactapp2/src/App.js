6650e9dc4cee07f24109ce85672ab44f5f501637
Ayshu-devi <150216493+Ayshu-devi@users.noreply.github.com>
2023-11-24 09:11:33 +0530

    DAY5

---
+++ DAY5/reactapp2/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Fundlifecycle from './components/Fundlifecycle';
+function App() {
+  return (
+    <div className="App">
+      <Fundlifecycle/>
+    </div>
+  );
+}
+
+export default App;
